14471a5d316439b85ddbf9e575d21411650d357c
user name <user email>
2023-12-29 21:45:13 -0500

    adjusted game rules, index, and script files

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ Web-Browser_Project-Hangman
 Rules to the game:
 Player has a chance to get the corrcect name of a sports athlete by inpuiting a single letter at a time.
 Player will be given a hint.
-Player has a certain amount of times to guess the right letter; everytime player guesses wrong, player loses numbe of chances to guess the letter until number of chances are gone.
+Player has a certain amount of times to guess the right letter; everytime player guesses wrong, player loses number of chances to guess the letter until number of chances are gone.
+While number of chances are diplayed, a body part will also be displayed for each wrong letter(head, body,left arm, right arm, left leg,right leg)until entire body is displayed which also equals total number of chances gone.
 When number of chances are gone, the player loses.
 If player gusses the athlete, player wins.
 Player can choose to play again(a new game) if he/she wins or loses.
--- script.js
+++ script.js
@@ -1,64 +1,89 @@
 
 
 
- const letterDiv = document.getElementById('letter');
- const wordDiv = document.getElementById('letter-input-sec');
- const hintDiv= document.getElementById('hint');
- const liveSpan =  document.querySelector('.lives');
- const hiddenNotification = document.getElementById('hidden-notification');
- const winText = document.getElementById('win-text');
- const notificationSpan = document.getElementById('notification-span');
- const newGameButton = document.getElementById('new-game-button');
+ 
+
+const words = ['KOBE', 'LEBRON', 'PRESCOTT', 'SHAQ', 'OHTANI'];
+
+let hangmnParts = []
+ 
+ let selectedWord = words[Math.floor(Math.random() * words.length)];
+ let remainingGuesses = 5;
+ let guessedLetters = [];
+ let wordDisplay = document.getElementById('wordDisplay');
+ let guessesLeft =document.getElementById('guessesLeft');
+ let letterButtons = document.getElementById('letterButtons');
 
 
- let lives = 6;
+ function displayWord() {
+  let wordHTML ='';
+  for (let letter of selectedWord) {
+    if (guessedLetters.includes(letter)) {
+      wordHTML += `<span>${letter}</span>`;
+    } else{
+       wordHTML +='<span>_</span>';
+    }
+   }
+   wordDisplay.innerHTML = wordHTML;
+}
+function displayGuessedLetters(){
+  letterButtons.innerHTML = '';
+   for (let i = 65; i <= 90; i++){
+    let letter = String.fromCharCode(i);
+    let button =  document.createElement('button');
+    button.textContent = letter;
+    button.onclick = function() {
+    checkLetter(letter);
+    button.disabled = true;
+  };
+letterButtons.appendChild(button);
+ }
+}
+function checkLetter(letter){
+  if(selectedWord.includes(letter)) {
+   guessedLetters.push(letter);
+} else {
+     remainingGuesses--;
+    }
+    displayWord();
+    displayGuessesLeft();
+    checkGameStatus();
+}
+
+ function displayGuessesLeft(){
+    guessesLeft.textContent = remainingGuesses;
+}
+
+function checkGameStatus() {
+     if (remainingGuesses === 0) {
+        endGame('Game Over! You Lose! The word was: ' + selectedWord);
+    } else if(!wordDisplay.textContent.includes('_')) {
+       endGame('Congrats! You guessed the right athlete: ' + selectedWord);
+    }
+
+}
+
+function endGame(message) {
+   alert(message);
+   resetGame();
+
+}
+
+function resetGame(){
+selectedWord = words[Math.floor(Math.random() *words.length)];
+remainingGuesses = 5;
+guessedLetters = [];
+displayWord();
+displayGuessesLeft();
+displayGuessedLetters();
+}
+
+//Initializing the game
+displayWord();
+displayGuessesLeft();
+displayGuessedLetters();
+
+
  
 
 
-//map array that holds athlete's names and hint:(key,and value paired)
- let athletes = new Map([
- ['Kobe',  'one of the greatest shooting guards in NBA history'],
- ['Lebron', 'the leading scorer in NBA history'],
- ['Prescott', 'current Quarteback for Cowboys'],
- ['Shaq', 'one of the most dominant big men in NBA history'],
- ['Emmit', 'NFL all time leaading rusher']]);
-
- const athletesList = [...athletes.keys()]; // use of spread operator. .keys() returns an iterableIterator (string) of keys in the map
-
- // get a random athlete from athletesList function
-const getRandomAthlete = function (list) {
-       return list[Math.floor(Math.random() * athletesList.length)];
-}
-
-//random athlete selected upon new game and initial
-let selectAthlete;
-let letters;
-
-const init = function (state){
-   wordDiv.innerHTML = '';
-   if (state === 'start'){
-//put letters into html
-       for (const i of 'abcdefghijklmnopqrstuvwxyz' ){
-           const html = `<button class = "alpha">${i.toUpperCase()}</button>`;
-           letterDiv.insertAdjacementHTML('beforeened',html);
-    }
-   } else if (state === 'new-game') {
-      letters.forEach(btn => {
-        btn.classList.remove('disabled');    
-    });
-    hintDiv.classList.add('hidden');
-    hiddenNotification.classList.add('hidden');
-  }
-};
-
-//capturing letters div
-letters = document.querySelectorAll('.alpha');
-liveSpan.textContent = lives;
-
-selectWord = getRandomAthlete(athletesList);
-for (let i = 0; i , athletesList.length; i++) {
-     const html = `p class="word">_</p>`;
-    wordDiv.insertAdjacementHTML('beforeend', html);
-}
-
-init('start')
