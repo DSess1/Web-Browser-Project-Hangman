719d0783acf159c8f752a0d4edf63634ba1900df
user name <user email>
2024-01-05 00:08:47 -0500

    polishing up on css, html, js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
- //athletes along with hints array
+ //Athletes' Names to be guessed ( with hints ) array
 
 const wordsWithHints = [{word:'KOBE',hint:'   Former Lakers great shooting guard of the NBA.'},
                         {word:'LEBRON',hint:'  The NBA all time leading scorer.'},
@@ -11,12 +11,10 @@ const wordsWithHints = [{word:'KOBE',hint:'   Former Lakers great shooting guard
                         {word:'BRADY', hint: '  Ex Patriots QB of NFL'},
                         {word:'JUDD', hint:  '  Current NY Yankees homerun hitter'},
                         {word:'MAHOMES', hint: '  Currnet NFL QB of KC Chiefs'}];
-
-
-
-                    
+                 
  
- let selectedWordInfo = wordsWithHints[Math.floor(Math.random() * wordsWithHints.length)];
+ let selectedWordInfo = wordsWithHints[Math.floor(Math.random() * wordsWithHints.length)];//Math.floor method returns greatest interger 
+ //less than or equal to argument....Math.random returns random number in wordHints array
  let selectedWord = selectedWordInfo.word;
  let wordHint = selectedWordInfo.hint;
  let remainingGuesses = 6;
@@ -29,46 +27,41 @@ const wordsWithHints = [{word:'KOBE',hint:'   Former Lakers great shooting guard
  function displayWord() {
   let wordHTML ='';
   for (let letter of selectedWord) {
-    if (guessedLetters.includes(letter)) {
-      wordHTML += `<span>${letter}</span>`;
-    } else{
-       wordHTML +='<span>_</span>';
+    if (guessedLetters.includes(letter)) {//checks whether guessed letter is included in selected word
+      wordHTML += `<span>${letter}</span>`;//places the value of letter directly into the string
+    } else {
+       wordHTML += '<span>_</span>';// displays the underlined values of each letter
     }
    }
-   wordDisplay.innerHTML = wordHTML;
+   wordDisplay.innerHTML = wordHTML;//displays word from the element property
 }
-
 
 // function to display guessed letter
 
 function displayGuessedLetters(){
   letterButtons.innerHTML = '';
-   for (let i = 65; i <= 90; i++){
-    let letter = String.fromCharCode(i);
-    let button =  document.createElement('button');
+   for (let i = 65; i <= 90; i++){// capital letters from A to Z
+    let letter = String.fromCharCode(i);//returns the letter in the alphabet
+    let button =  document.createElement('button');  //creates a html button in js
     button.textContent = letter;
-    button.onclick = function() {
+    button.onclick = function() {//event hamdler button for function  when clicked
     checkLetter(letter);
     button.disabled = true;
    
   };
-letterButtons.appendChild(button);
+letterButtons.appendChild(button);//button becomes a new child of letterButtons (part of alphabets  displayed)
+console.log(button);
  }
 
 }
 
-
-
-
 // function takes in guessed letter as an argument. Checks to see if the slected word being
-// guessed contains the guessed letter.
+// guessed contains a correct letter.
 function checkLetter(letter){
   if(selectedWord.includes(letter)) {
    guessedLetters.push(letter);// if letter is correct it is pushed in guessed letter array
 } else {
      remainingGuesses--;  // if guessed letter incorrect it decrements(--) the remainingGuesses counter
-   
-   
     }
     displayWord();
     displayGuessesLeft();
@@ -84,9 +77,9 @@ function checkLetter(letter){
 
 // function to check and display Win or Lose
 function checkGameStatus() {
-     if (remainingGuesses === 0) {
+     if (remainingGuesses === 0) {// checks for remaining guesses.. if remaining guesses = 0, executes endgame function resulting in an unsucessful game.
         endGame('Game Over! You Lose! The word was: ' + selectedWord);
-    } else if(!wordDisplay.textContent.includes('_')) {
+    } else if(!wordDisplay.textContent.includes('_')) {///checks to see if underscore is included, if underscore is not included it executes the endgame function for a sucessful game
        endGame('Congrats! You guessed the right athlete: ' + selectedWord);
     }
 
@@ -94,9 +87,7 @@ function checkGameStatus() {
 // function end game 
 function endGame(message) {
    alert(message);
-   resetGame();
-   
-
+  // resetGame();
 }
 
 //reset game function along with hint
@@ -113,12 +104,9 @@ displayGuessedLetters();
 alert('Hint:' + wordHint);// display hint  when game is reset
 
 }
+resetGame();
 
-//Initializing the game
-displayWord();
-displayGuessesLeft();
-displayGuessedLetters();
-alert('Hint:' + wordHint);
+
 
 
 
